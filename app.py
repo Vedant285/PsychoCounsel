4181a360f397c47fc758214e2dcfdd66afd3ad8a
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-01-19 17:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,19 +211,19 @@ with counsel_tab:
         useEmotion = False
         st.info('Emotion Tracking Report not Available!')
     
-    personalize = st.toggle('Personalize information?')
-    if personalize:
-        with st.expander('Personalization'):
-            name = st.text_input('Name')
+   # personalize = st.toggle('Personalize information?')
+   # if personalize:
+       # with st.expander('Personalization'):
+          #  name = st.text_input('Name')
        #     age = st.number_input('Age', 1, 100)
         #    gender = st.radio('Gender', ['Male', 'Female'], horizontal=True)
 
-        p_info = f'Name: {namme}'
+       # p_info = f'Name: {namme}'
 
-    tell = st.toggle("Tell me what's on your mind?")
-    user_input = "" 
-    if tell:
-        with st.expander('User Input'):
+    #tell = st.toggle("Tell me what's on your mind?")
+    #user_input = "" 
+    #if tell:
+       # with st.expander('User Input'):
       #      mode = st.radio('Mode', ['Speak', 'Type'])
        #     if mode == 'Speak':
                 # Build custom audio recorder widget
@@ -239,8 +239,8 @@ with counsel_tab:
               #      st.write(user_input)
                #     os.remove(file_name)
 
-            else:
-                user_input = st.text_area('Text to Analyze')
+            #else:
+                #user_input = st.text_area('Text to Analyze')
 
     # If minimum options selected
     if useEmotion or useHeart :
@@ -253,13 +253,13 @@ with counsel_tab:
             response = llm_chain.run(
                 emotion_report=st.session_state.report['emotion'] if useEmotion else None,
                 heart_report=st.session_state.report['heart'] if useHeart else None,
-                p_info=p_info if personalize else None,
+                #p_info=p_info if personalize else None,
                # thoughts=user_input if tell else None,
 
             )
             
-            #wait.empty()
-            #st.write(response)
+            wait.empty()
+            st.write(response)
 
             # Text to Speech
             #speech_bytes = BytesIO()
